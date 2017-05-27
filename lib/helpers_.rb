2fa34a25013399fed8024e278e57e5d50444775f
Denis Defreyne <denis.defreyne@stoneship.org>
2017-05-27 14:01:42 +0200

    Remove unused lib/ files

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -1,12 +1,5 @@
-# Helpers - built-in
 include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::HTMLEscape
 include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::XMLSitemap
-
-# Helpers - custom
-include StoneshipSite::Helpers::HTMLSitemap
-include StoneshipSite::Helpers::DemoteHeaders
-include StoneshipSite::Helpers::Indenting
-include StoneshipSite::Helpers::Ratings
--- lib/resize_filter.rb
+++
@@ -1,8 +0,0 @@
-Class.new(Nanoc::Filter) do
-  identifier :resize
-  type binary: :binary
-
-  def run(filename, params = {})
-    system('sips', '--resampleWidth', params.fetch(:width).to_s, '--out', output_filename, filename)
-  end
-end
