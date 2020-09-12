20d316f555e5c3649fa06580e1e25a8839138eee
Denis Defreyne <denis.defreyne@stoneship.org>
2020-09-12 09:33:19 +0200

    Add new CV

---
+++ lib/tailwindcss.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+Nanoc::Filter.define(:tailwindcss) do |content, _params = {}|
+  Tempfile.create(['src', '.css']) do |src_file|
+    File.write(src_file, content)
+
+    Tempfile.create(['dst', '.css']) do |dst_file|
+      cmd = [
+        'npx',
+        'tailwindcss',
+        'build',
+        src_file.path,
+        '-o',
+        dst_file.path,
+        '--config',
+        'tailwind.config.js'
+      ]
+
+      command = TTY::Command.new(printer: :null)
+      command.run(*cmd, input: '').out
+
+      File.read(dst_file)
+    end
+  end
+end
