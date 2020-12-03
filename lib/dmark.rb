9d7c9ec0ec27d36af583c79787b83b6a8d0e8f1b
Denis Defreyne <denis.defreyne@stoneship.org>
2020-12-03 12:12:36 +0100

    Refactor: Merge lib/errors.rb into lib/dmark.rb

--- lib/dmark.rb
+++ lib/dmark.rb
@@ -150,5 +150,20 @@ Nanoc::Filter.define(:dmark) do |content, _params = {}|
   context = { items: @items, item: @item, config: @config, nodes: nodes }
   DMarkTranslator.translate(nodes, context)
 rescue StandardError => e
-  handle_error(e, content)
+  case e
+  when DMark::Parser::ParserError
+    line = content.lines[e.line_nr]
+
+    lines = [
+      e.message,
+      '',
+      line,
+      "\e[31m" + ' ' * [e.col_nr - 1, 0].max + '↑' + "\e[0m",
+    ]
+
+    fancy_msg = lines.map { |l| "\e[34m[D*Mark]\e[0m #{l.rstrip}\n" }.join('')
+    raise "D*Mark parser error\n\n" + fancy_msg
+  else
+    raise e
+  end
 end
