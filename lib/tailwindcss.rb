cb8f09a7d8cfa5859291f7481cf644adc31b2429
Denis Defreyne <denis.defreyne@stoneship.org>
2021-01-10 12:02:28 +0100

    Set up tailwind.config.js dependency
    This doesn’t yet work with nanoc-live unfortunately

--- lib/tailwindcss.rb
+++ lib/tailwindcss.rb
@@ -1,11 +1,28 @@
 # frozen_string_literal: true
 
+class TailwindConfig < ::Nanoc::DataSource
+  identifier :tailwind_config
+
+  def items
+    [
+      new_item(
+        File.read('tailwind.config.js'),
+        {},
+        '/ignore/tailwind.config.js'
+      )
+    ]
+  end
+end
+
 Nanoc::Filter.define(:tailwindcss) do |content, _params = {}|
   # Set up dependencies for purgecss
   ENV['NODE_ENV'] = 'production'
   @items.find_all('/**/*.{html,dmark,md}').each do |item|
     item.compiled_content
   end
+
+  # Set up dependency on tailwind.config.js
+  @items['/ignore/tailwind.config.js'].raw_content
 
   Tempfile.create(['src', '.css']) do |src_file|
     File.write(src_file, content)
