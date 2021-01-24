8c24e86bdacdc3d34b9b36ea63102e43fe2a77a0
Denis Defreyne <denis.defreyne@stoneship.org>
2021-01-24 11:51:47 +0100

    Use sans serif for headings

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
       decimal: "decimal",
     },
     fontFamily: {
+      sans: ["PT Sans", "Helvetica", "Arial", "sans-serif"],
       serif: ["PT Serif", "Georgia", "serif"],
     },
     extend: {
