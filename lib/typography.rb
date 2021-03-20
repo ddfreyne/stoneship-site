a72fdcb5633d80616c8d93c648c6ce473551f4b0
Denis Defreyne <denis.defreyne@stoneship.org>
2021-03-20 12:03:18 +0100

    Add thin space around em dashes

--- lib/typography.rb
+++ lib/typography.rb
@@ -5,7 +5,7 @@ Nanoc::Filter.define(:typography) do |content, _params = {}|
 
   doc.css('p').each do |para|
     para.children.each do |child|
-      next child unless child.text?
+      next unless child.text?
 
       # Replace non-breaking space
       child.content = child.content.sub(/ ([^ ]+)\z/, ' \1')
@@ -14,6 +14,13 @@ Nanoc::Filter.define(:typography) do |content, _params = {}|
 
   doc.css('.path').each do |path|
     path.content = path.content.gsub(/ /, ' ').gsub('/', "\u200B/")
+  end
+
+  doc.traverse do |elem|
+    next unless elem.text?
+
+    # wrap em dashes with thin spaces
+    elem.content = elem.content.gsub(/\s*—\s*/, "\u2009—\u2009")
   end
 
   doc.to_s
