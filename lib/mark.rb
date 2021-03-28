76982b0032f2bec8f132c55843ffcbe7ac61dffd
Denis Defreyne <denis.defreyne@stoneship.org>
2021-03-28 10:25:35 +0200

    Clean up wikilinks+mark filters

--- lib/mark.rb
+++ lib/mark.rb
@@ -5,7 +5,15 @@ Nanoc::Filter.define(:mark) do |content, _params = {}|
 
   content
     .gsub(mark_regex) do |match|
-      marked_content = match[2..-3]
-      '<mark>' + ::Kramdown::Document.new(marked_content).to_html.gsub(%r{</?p>}, '') + '</mark>'
+      # Get contents between :: pair
+      marked_raw = match[2..-3]
+
+      # Convert from Markdown to HTML
+      marked_html = ::Kramdown::Document.new(marked_raw).to_html
+
+      # Strip surrounding <p> and </p>
+      marked_html_clean = marked_html.gsub(%r{</?p>}, '')
+
+      "<mark>#{marked_html_clean}</mark>"
     end
 end
--- lib/wikilinks.rb
+++ lib/wikilinks.rb
@@ -3,11 +3,17 @@
 Nanoc::Filter.define(:wikilinks) do |content, _params = {}|
   content
     .gsub(/\[\[[^\]]+\]\]/) do |match|
+      # Get contents between [ and ]
       title = match[2..-3]
+
+      # Handle empty-nested-array case (special case)
       next if title.strip.empty?
 
+      # Find note with matching title
       link_target = @items.find_all('/notes/*').find { |i| i[:title] == title }
+
       if link_target
+        # Convert to Markdown link
         "[#{title}](#{link_target.reps[:default].path})"
       else
         raise "Broken reference to #{title}"
