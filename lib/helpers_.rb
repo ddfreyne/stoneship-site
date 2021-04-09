bf8d85eedd9d6d512488e55f152ef86508ce32f2
Denis Defreyne <denis.defreyne@stoneship.org>
2021-04-09 22:13:09 +0200

    Mark blooming notes

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -4,3 +4,11 @@ include Nanoc::Helpers::XMLSitemap
 include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::Breadcrumbs
+
+def classes_for_note_li(note)
+  if note[:tags].include?('#stage/blooming')
+    'list-icon-star'
+  else
+    ''
+  end
+end
