542453d1d18086685b46d5601b35ff3209e3cef3
Denis Defreyne <denis.defreyne@stoneship.org>
2021-05-17 21:46:19 +0200

    Remove unused helper

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -4,11 +4,3 @@ include Nanoc::Helpers::XMLSitemap
 include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::Breadcrumbs
-
-def classes_for_note_li(note)
-  if note[:tags].include?('#stage/blooming')
-    'list-icon-star'
-  else
-    ''
-  end
-end
