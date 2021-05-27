eb596e23359e01e35283936f5402d8e7c032cef1
Denis Defreyne <denis.defreyne@stoneship.org>
2021-05-27 22:02:25 +0200

    Handle run-in headers

---
+++ lib/run_in_headers.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'nokogiri'
+
+Class.new(Nanoc::Filter) do
+  identifiers :run_in_headers
+
+  def run(content, _arguments = {})
+    doc = Nokogiri::HTML.fragment(content)
+    doc.css('h3 + p').each do |para|
+      h3 = para.previous
+      while h3.text?
+        h3 = h3.previous
+      end
+
+      div = Nokogiri::XML::Node.new('div', doc)
+      div['class'] = 'run-in'
+      div.add_child(h3.dup)
+      div.add_child(para.dup)
+
+      para.add_next_sibling(div)
+
+      h3.unlink
+      para.unlink
+
+      puts
+    end
+    doc.to_s
+  end
+end
