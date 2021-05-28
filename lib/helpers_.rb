b5166f7b545fd0b854763fd10b082126744862b2
Denis Defreyne <denis.defreyne@stoneship.org>
2021-05-28 10:45:06 +0200

    Don’t index articles/fiction

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -4,3 +4,21 @@ include Nanoc::Helpers::XMLSitemap
 include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::Rendering
 include Nanoc::Helpers::Breadcrumbs
+
+def meta_robots
+  values = []
+
+  if @item[:index].equal?(false)
+    values << 'noindex'
+  end
+
+  if @item[:follow].equal?(false)
+    values << 'nofollow'
+  end
+
+  if values.any?
+    %[<meta name="robots" content="#{values.join(', ')}">]
+  else
+    ''
+  end
+end
